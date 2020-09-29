a60c0dd2f2f796c682d1dfd0feb93724367b8b3a
Tim Akermann <ti.akermann@gmail.com>
2020-09-29 10:27:16 +0200

    Version Change

--- src/OpenFL.Editor.Examples/OpenFLExamplesPlugin.cs
+++ src/OpenFL.Editor.Examples/OpenFLExamplesPlugin.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
+using OpenFL.Editor.Forms;
+using OpenFL.Editor.Utils.Plugins;
 using OpenFL.Examples;
 
 using PluginSystem.Core.Pointer;
 using PluginSystem.Utility;
 
+using ThemeEngine.Forms;
+
+using Utility.IO.Callbacks;
 using Utility.IO.VirtualFS;
+using Utility.ProgressFeedback;
+using Utility.WindowsForms.CustomControls;
 
 namespace OpenFL.Editor.Examples
 {
@@ -23,6 +34,58 @@ namespace OpenFL.Editor.Examples
 
             ManifestReader.RegisterAssembly(typeof(ExampleInformation).Assembly);   
         }
+
+        [ToolbarItem("Examples")]
+        private void Examples()
+        {
+
+        }
+
+        [ToolbarItem("Examples/Unpack Examples")]
+        private void UnpackExamples()
+        {
+            ProgressIndicator.RunTask(UnpackResources, Application.DoEvents);
+            if (StyledMessageBox.Show(
+                                      "Unpacking Finished.",
+                                      "Do you want to load view the Unpacked Files?",
+                                      MessageBoxButtons.YesNo, SystemIcons.Question
+                                     ) ==
+                DialogResult.Yes)
+            {
+                Process.Start(".\\examples\\");
+            }
+        }
+
+        private void UnpackResources(IProgressIndicator indicator)
+        {
+            string workingDir = FLScriptEditor.Settings.WorkingDir ?? Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(Application.StartupPath);
+            string[] files = IOManager.GetFiles("examples");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                indicator.SetProgress("Unpacking file: " + file, i, files.Length - 1);
+                string dir = Path.GetDirectoryName(file);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                if (!File.Exists(file))
+                {
+                    Stream s = IOManager.GetStream(file);
+                    Stream dst = File.Create(file);
+                    s.CopyTo(dst);
+                    s.Dispose();
+                    dst.Dispose();
+                }
+            }
+
+            Directory.SetCurrentDirectory(workingDir);
+        }
+        
+
 
     }
 }
