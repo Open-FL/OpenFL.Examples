8e6f90f7f34128cb807a07ea80faaf2e64e35485
Tim Akermann <ti.akermann@gmail.com>
2020-09-29 10:04:12 +0200

    Initial Commit.

---
+++ src/OpenFL.Editor.Examples/OpenFLExamplesPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenFL.Examples;
+
+using PluginSystem.Core.Pointer;
+using PluginSystem.Utility;
+
+using Utility.IO.VirtualFS;
+
+namespace OpenFL.Editor.Examples
+{
+    public class OpenFLExamplesPlugin: APlugin<FLEditorPluginHost>
+    {
+
+        public override void OnLoad(PluginAssemblyPointer ptr)
+        {
+            base.OnLoad(ptr);   
+
+            ManifestReader.RegisterAssembly(typeof(ExampleInformation).Assembly);   
+        }
+
+    }
+}
---
+++ src/OpenFL.Examples/ExampleInformation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenFL.Examples
+{
+   public class ExampleInformation
+    {
+    }
+}
